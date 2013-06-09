12780db53a8832f0e06268b4b39b6e0d3e18680c
Harm Hanemaaijer <fgenfb@yahoo.com>
2013-06-09 20:37:16 +0200

    Pull in lastest ARM-optimized memcpy from fastarm repository
    Although the assembler optimized memcpy functions are not yet really
    utitized, they show performance roughly comparable with pixman when
    called from a blit function line by line. They may be adapted in the
    future in the form of a dedicated blit function.
    
    Because compile time flags have changed, this commit requires
    rernnning autoreconf and configure.

--- src/arm_asm.h
+++ src/arm_asm.h
@@ -1,5 +1,114 @@
 
 extern void *memcpy_armv5te(void *dest, const void *src, int n);
 
+#ifdef RPI_BEST_MEMCPY_ONLY
+
 extern void *memcpy_armv5te_no_overfetch(void *dest, const void *src, int n);
 
+extern void *memcpy_armv5te_overfetch(void *dest, const void *src, int n);
+
+#else
+
+extern void *memcpy_armv5te_no_overfetch_align_16_block_write_8_preload_96(void *dest,
+    const void *src, int n);
+
+extern void *memcpy_armv5te_no_overfetch_align_16_block_write_16_preload_96(void *dest,
+    const void *src, int n);
+
+extern void *memcpy_armv5te_no_overfetch_align_16_block_write_16_preload_early_96(void *dest,
+    const void *src, int n);
+
+extern void *memcpy_armv5te_no_overfetch_align_16_block_write_16_preload_early_128(void *dest,
+    const void *src, int n);
+
+extern void *memcpy_armv5te_no_overfetch_align_32_block_write_8_preload_96(void *dest,
+    const void *src, int n);
+
+extern void *memcpy_armv5te_no_overfetch_align_32_block_write_16_preload_64(void *dest,
+    const void *src, int n);
+
+extern void *memcpy_armv5te_no_overfetch_align_32_block_write_16_preload_96(void *dest,
+    const void *src, int n);
+
+extern void *memcpy_armv5te_no_overfetch_align_32_block_write_16_preload_128(void *dest,
+    const void *src, int n);
+
+extern void *memcpy_armv5te_no_overfetch_align_32_block_write_16_preload_160(void *dest,
+    const void *src, int n);
+
+extern void *memcpy_armv5te_no_overfetch_align_32_block_write_16_preload_192(void *dest,
+    const void *src, int n);
+
+extern void *memcpy_armv5te_no_overfetch_align_32_block_write_16_preload_256(void *dest,
+    const void *src, int n);
+
+extern void *memcpy_armv5te_no_overfetch_align_32_block_write_32_preload_64(void *dest,
+    const void *src, int n);
+
+extern void *memcpy_armv5te_no_overfetch_align_32_block_write_32_preload_96(void *dest,
+    const void *src, int n);
+
+extern void *memcpy_armv5te_no_overfetch_align_32_block_write_32_preload_128(void *dest,
+    const void *src, int n);
+
+extern void *memcpy_armv5te_no_overfetch_align_32_block_write_32_preload_160(void *dest,
+    const void *src, int n);
+
+extern void *memcpy_armv5te_no_overfetch_align_32_block_write_32_preload_192(void *dest,
+    const void *src, int n);
+
+extern void *memcpy_armv5te_no_overfetch_align_32_block_write_32_preload_256(void *dest,
+    const void *src, int n);
+
+extern void *memcpy_armv5te_no_overfetch_align_32_block_write_16_preload_early_96(void *dest,
+    const void *src, int n);
+
+extern void *memcpy_armv5te_no_overfetch_align_32_block_write_16_preload_early_128(void *dest,
+    const void *src, int n);
+
+extern void *memcpy_armv5te_no_overfetch_align_32_block_write_16_preload_early_192(void *dest,
+    const void *src, int n);
+
+extern void *memcpy_armv5te_no_overfetch_align_32_block_write_16_preload_early_256(void *dest,
+    const void *src, int n);
+
+extern void *memcpy_armv5te_no_overfetch_align_32_block_write_32_preload_early_128(void *dest,
+    const void *src, int n);
+
+extern void *memcpy_armv5te_no_overfetch_align_32_block_write_32_preload_early_192(void *dest,
+    const void *src, int n);
+
+extern void *memcpy_armv5te_no_overfetch_align_32_block_write_32_preload_early_256(void *dest,
+    const void *src, int n);
+
+extern void *memcpy_armv5te_no_overfetch_align_32_block_write_16_no_preload(void *dest,
+    const void *src, int n);
+
+extern void *memcpy_armv5te_no_overfetch_align_32_block_write_32_no_preload(void *dest,
+    const void *src, int n);
+
+extern void *memcpy_armv5te_no_overfetch_line_64_align_32_block_write_32_preload_early_128(void *dest,
+    const void *src, int n);
+
+extern void *memcpy_armv5te_no_overfetch_line_64_align_32_block_write_32_preload_early_192(void *dest,
+    const void *src, int n);
+
+extern void *memcpy_armv5te_no_overfetch_line_64_align_32_block_write_32_preload_early_256(void *dest,
+    const void *src, int n);
+
+extern void *memcpy_armv5te_no_overfetch_line_64_align_32_block_write_32_preload_early_320(void *dest,
+    const void *src, int n);
+
+extern void *memcpy_armv5te_no_overfetch_line_64_align_64_block_write_32_preload_early_256(void *dest,
+    const void *src, int n);
+
+extern void *memcpy_armv5te_no_overfetch_line_64_align_64_block_write_32_preload_early_320(void *dest,
+    const void *src, int n);
+
+extern void *memcpy_armv5te_overfetch_align_16_block_write_16_preload_early_128(void *dest,
+    const void *src, int n);
+
+extern void *memcpy_armv5te_overfetch_align_32_block_write_32_preload_early_192(void *dest,
+    const void *src, int n);
+
+#endif
